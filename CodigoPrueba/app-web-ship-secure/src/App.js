2611e988415bfdcdb0a8472687af0f1587086955
Cristian <cmsider@hotmail.com>
2021-05-27 00:38:36 -0300

    agregada lista desplegable

--- CodigoPrueba/app-web-ship-secure/src/App.js
+++ CodigoPrueba/app-web-ship-secure/src/App.js
@@ -2,18 +2,14 @@
 import './App.css';*/
 //import Jsx from './components/Jsx' va abajo del FormularioEnvio-->      <Jsx />
 //import Lista from './components/Lista' va abajo delFormularioEnvio--> <Lista />
-import FormularioEnvio from './components/FormularioEnvio'
-import FormHook from './components/FormHook'
-import FormCrud from './components/FormCrud'
-
+//import FormularioEnvio from "./components/FormularioEnvio";
+//import FormHook from "./components/FormHook";
+import FormCrud from "./components/FormCrud";
 
 function App() {
   return (
-    <div className="container mt-5">
-     <FormularioEnvio/>
-     <FormHook/>
-     <FormCrud/>
-
+    <div className="container mt-5" className="fondoFormEnvios">
+      <FormCrud />
     </div>
   );
 }
--- CodigoPrueba/app-web-ship-secure/src/components/FormCrud.js
+++ CodigoPrueba/app-web-ship-secure/src/components/FormCrud.js
@@ -1,5 +1,11 @@
 import React, { Fragment, useState } from "react";
 import { useForm } from "react-hook-form";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
 
 const FormCrud = () => {
   const {
@@ -7,6 +13,7 @@ const FormCrud = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+
   const [entradas, setentradas] = useState([]);
 
   const procesarFormulario = (data, e) => {
@@ -15,54 +22,88 @@ const FormCrud = () => {
     // limpiar campos
     e.target.reset();
   };
+  const [dropdown, setDropdown] = useState(false);
+  const abrirCerrarDropdown = () => {
+    setDropdown(!dropdown);
+  };
+
+  const accionDropDown = () => {
+    alert("Evento on click dropdown");
+  };
 
   return (
-    <Fragment>
-      <h1>FORM</h1>
-      <form onSubmit={handleSubmit(procesarFormulario)}>
-        <input
-          name="nombre"
-          {...register("nombre", {
-            required: { value: true, message: "Campo requerido" },
-            minLength: {
-              value: 2,
-              message: "El nombre debe tener un minimo de 2 letras",
-            },
-          })}
-          className="form-control my-2"
-          placeholder="Ingrese nombre"
-        ></input>
-        <span className="text-danger text-small d-block mb-2">
-          {errors?.nombre?.message}
-          {/*si da error en el nombre muestra el mensaje de error en nobmre*/}
-        </span>
-        <input
-          name="apellido"
-          {...register("apellido", {
-            required: { value: true, message: "Campo requerido" },
-            minLength: {
-              value: 2,
-              message: "El apellido debe tener un minimo de 2 letras",
-            },
-          })}
-          className="form-control my-2"
-          placeholder="Ingrese apellido"
-        ></input>
-        <span className="text-danger text-small d-block mb-2">
-          {errors?.apellido?.message}
-        </span>
-        <button type="submit" className="btn btn-primary">
-          Agregar
-        </button>
-      </form>
-      <ul className="mt-2">
-        {entradas.map((item, index) => (
-          <li key={index}>
-            {item.nombre} - {item.apellido}
-          </li>
-        ))}
-      </ul>
-    </Fragment>
+    <body className="fondoFormEnvios">
+      <Fragment>
+        <h1>FORM</h1>
+        <form onSubmit={handleSubmit(procesarFormulario)}>
+          <input
+            name="nombre"
+            {...register("nombre", {
+              required: { value: true, message: "Campo requerido" },
+              minLength: {
+                value: 2,
+                message: "El nombre debe tener un minimo de 2 letras",
+              },
+            })}
+            className="form-control my-2"
+            placeholder="Ingrese nombre"
+          ></input>
+          <span className="text-danger text-small d-block mb-2">
+            {errors?.nombre?.message}
+            {/*si da error en el nombre muestra el mensaje de error en nobmre*/}
+          </span>
+          <input
+            name="apellido"
+            {...register("apellido", {
+              required: { value: true, message: "Campo requerido" },
+              minLength: {
+                value: 2,
+                message: "El apellido debe tener un minimo de 2 letras",
+              },
+            })}
+            className="form-control my-2"
+            placeholder="Ingrese apellido"
+          ></input>
+          <span className="text-danger text-small d-block mb-2">
+            {errors?.apellido?.message}
+          </span>
+          <Dropdown isOpen={dropdown} toggle={abrirCerrarDropdown}>
+            <DropdownToggle
+              caret
+              className="botonDropdown"
+              className="left 250px"
+              className="weight 250px"
+            >
+              Dropdown Ejemplo
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Encabezado</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Accion 1</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem onClick={() => accionDropDown()}>
+                Accion 2
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Accion 3</DropdownItem>
+              <DropdownItem divider />
+
+              <DropdownItem disabled>OPT(deshabilitada)</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <button type="submit" className="btn btn-primary">
+            Agregar
+          </button>
+        </form>
+        <ul className="mt-2">
+          {entradas.map((item, index) => (
+            <li key={index}>
+              {item.nombre} - {item.apellido}
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    </body>
   );
 };
 
